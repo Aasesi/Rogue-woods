f7f54e9b9f23be7cd0bccb409807a3a881cd87c2
Aasesi <mikolajwilczynnski@gmail.com>
2023-05-11 14:52:12 +0200

    Console completed

--- src/console.cpp
+++ src/console.cpp
@@ -4,19 +4,18 @@ Console::Console()
 {
 	if (!_font.loadFromFile(font_path))
 	{
-		
 	}
 	_text.setFont(_font);
 	_text.setCharacterSize(24);
 	_text.setPosition(sf::Vector2f(95.f, 200.f));
 	_text.setFillColor(sf::Color::White);
 
-	dir=None;
+	dir = None;
 }
 
-void Console::handle_input(sf::Event& event, sf::RenderWindow& window, sf::Vector2f mousepos)
+void Console::handle_input(sf::Event &event, sf::RenderWindow &window, sf::Vector2f mousepos)
 {
-if (is_full && contains(mousepos))
+	if (is_full && contains(mousepos))
 	{
 		if (event.type == sf::Event::MouseWheelScrolled)
 		{
@@ -67,5 +66,84 @@ void Console::update()
 	else
 	{
 		dir = None;
+	}
+}
+
+void Console::get_focus()
+{
+	if (is_full)
+	{
+		int some_size = down_lines.size();
+		for (int i = 0; i < some_size; i++)
+		{
+			console_text += down_lines.back();
+			down_lines.pop_back();
+			size_t target_pos = console_text.find("\n") + 1;
+			std::string new_line = console_text.substr(0, target_pos);
+			console_text.erase(0, target_pos);
+			up_lines.push_back(new_line);
+		}
+	}
+}
+
+void Console::add_new_text(std::string new_text)
+{
+	if (!new_text.empty())
+	{
+		const std::string character = "\n";
+		// I used lambda but remember if you wanted make it better instead of char c use string because right now it just searches for the '\'
+		int substringcount = std::count_if(new_text.begin(), new_text.end(), [character](char c)
+										   { return c == character[0]; });
+		curr_lines += substringcount;
+
+		if (curr_lines >= number_of_lines && !is_scrolled())
+		{
+			console_text += new_text;
+			is_full = true;
+			for (int i = 0; i < substringcount; i++)
+			{
+				size_t target_pos = console_text.find('\n');
+				std::string new_line = console_text.substr(0, target_pos + 1);
+				console_text.erase(0, target_pos + 1);
+				up_lines.push_back(new_line);
+				curr_lines--;
+			}
+			_text.setString(console_text);
+		}
+		else if (curr_lines >= number_of_lines && is_scrolled())
+		{
+			is_full = true;
+			get_focus();
+			console_text += new_text;
+			for (int i = 0; i < substringcount; i++)
+			{
+				size_t target_pos = console_text.find("\n");
+				std::string new_line = console_text.substr(0, target_pos + 1);
+				console_text.erase(0, target_pos + 1);
+				up_lines.push_back(new_line);
+				curr_lines--;
+			}
+			_text.setString(console_text);
+		}
+		else
+		{
+			console_text += new_text;
+			_text.setString(console_text);
+		}
+	}
+	else
+	{
+	}
+}
+
+bool Console::is_scrolled()
+{
+	if (down_lines.empty())
+	{
+		return false;
+	}
+	else
+	{
+		return true;
 	}
 }
